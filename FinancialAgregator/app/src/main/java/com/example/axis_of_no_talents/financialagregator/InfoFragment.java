327680f0a130d772132c5df6fc9283ae81966e2f
kwanat <wanat.kamil@wp.pl>
2017-11-29 11:28:16 +0100

    initial commit

---
+++ FinancialAgregator/app/src/main/java/com/example/axis_of_no_talents/financialagregator/InfoFragment.java
@@ -0,0 +1,13 @@
+package com.example.axis_of_no_talents.financialagregator;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
